98f1f68935570912ba5ff92ef8781c5043aefde6
BiblioTech <bibliotech@example.com>
2025-04-12 20:58:57 -0300

    Primeiro commit do projeto Incluir+Web

--- src/components/CardLetra.jsx
+++ src/components/CardLetra.jsx
@@ -1,23 +1,25 @@
-function CardLetra({letra,palavra,imagem,som}) {
+function CardLetra({ letra, palavra, imagem, som }) {
     const tocarSom = () => {
-        const audio = new Audio(som)
-        audio.play()
+      const audio = new Audio(som);
+      audio.play();
     };
-    return(
-        <div style={{
-            border:'2px solid #ccc',
-            borderRadius:'10px',
-            padding:'1rem',
-            textAlign:'center', 
-            maxWidth:'300px',
-            margin:'1rem auto'
-        }}>
-            <h3 style={{fontSize:'2rem'}}>{letra}</h3>
-            <img src={imagem} alt={palavra} style={{width:'100%'}}/>
-            <p>{palavra}</p>
-            <buton onClick={tocarSom}>Ouvir</buton>
-        </div>
+  
+    return (
+      <div style={{
+        border: '2px solid #ccc',
+        borderRadius: '10px',
+        padding: '1rem',
+        textAlign: 'center',
+        maxWidth: '300px',
+        margin: '1rem auto'
+      }}>
+        <h3 style={{ fontSize: '4rem',margin:'0' }}>{letra}</h3>
+        <img src={imagem} alt={palavra} style={{ width: '100%' }} />
+        <p style={{fontSize:'1.8rem',margin:"0.5rem 0"}}>{palavra}</p>
+        <button onClick={tocarSom}>Ouvir</button>
+      </div>
     );
-}
-
-export default CardLetra;
+  }
+  
+  export default CardLetra;
+  
